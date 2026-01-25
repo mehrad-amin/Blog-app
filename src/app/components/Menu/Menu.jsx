a0c0e910e7f101857ee624e700271dc24eb620ba
Mehrad aminsharei <mehradaminsharei@gmail.com>
2026-01-25 16:30:42 +0330

    container added in CardList & Menu components

--- src/app/components/Menu/Menu.jsx
+++ src/app/components/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 import styles from "@/app/components/Menu/Menu.module.css";
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return <div className={styles.container}>Menu</div>;
 };
 
 export default Menu;
