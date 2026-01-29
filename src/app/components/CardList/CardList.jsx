55bae10a5db2b482e5b99a2249924865e3647a20
Mehrad aminsharei <mehradaminsharei@gmail.com>
2026-01-29 15:33:43 +0330

    Pagination ui compleated

--- src/app/components/CardList/CardList.jsx
+++ src/app/components/CardList/CardList.jsx
@@ -1,18 +1,19 @@
 import styles from "@/app/components/CardList/CardList.module.css";
 import Image from "next/image";
 import Card from "@/app/components/Card/Card";
+import Pagination from "../Pagination/Pagination";
 const CardList = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
       <div className={styles.posts}>
         {/* Posts will be rendered here */}
-        <div className={styles.post}>
-          <Card />
-          <Card />
-          <Card />
-        </div>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
+      <Pagination />
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import CategoryList from "./components/CategoryList/CategoryList";
 import Featured from "./components/Featured/Featured";
 import styles from "@/app/page.module.css";
 import Menu from "./components/Menu/Menu";
+import Pagination from "./components/Pagination/Pagination";
 export default function Home() {
   return (
     <div className={styles.container}>
