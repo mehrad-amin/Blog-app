163b2da8e54b9c6c187e87ac5967e1d3e3f6d63c
Mehrad aminsharei <mehradaminsharei@gmail.com>
2026-01-29 16:22:12 +0330

    footer ui & media query compleated

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,7 +1,52 @@
 import styles from "@/app/components/Footer/Footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div className={styles.container}>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src="/logo.png" alt="Logo" width={50} height={50} />
+          <h1 className={styles.logoText}>My Blog</h1>
+        </div>
+        <p className={styles.description}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+          maxime reprehenderit tempora, molestias hic, repellat illum tenetur,
+          laudantium magnam at nulla similique ratione facere omnis.
+        </p>
+        <div className={styles.icons}>
+          <Image src="/facebook.png" alt="Facebook" width={18} height={18} />
+          <Image src="/youtube.png" alt="YouTube" width={18} height={18} />
+          <Image src="/instagram.png" alt="Instagram" width={18} height={18} />
+          <Image src="/tiktok.png" alt="TikTok" width={18} height={18} />
+        </div>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href="/">Home</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+          <Link href="/">Blog</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href="/">Style</Link>
+          <Link href="/">Fashion</Link>
+          <Link href="/">Coding</Link>
+          <Link href="/">Travel</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Tiktok</Link>
+          <Link href="/">Youtube</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
