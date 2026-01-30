e660b97720633ee3278c6f2942b9d2a4f52bc6ce
Mehrad aminsharei <mehradaminsharei@gmail.com>
2026-01-30 16:00:01 +0330

    Comments component added in proj

---
+++ src/app/components/Comment/Comments.jsx
@@ -0,0 +1,7 @@
+import styles from "@/app/components/Comment/Comments.module.css";
+
+const Comments = () => {
+  return <div className={styles.container}>Comments</div>;
+};
+
+export default Comments;
