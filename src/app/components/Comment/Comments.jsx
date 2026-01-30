b0862187cfa0d0ec4bedd94e8d38c71786eb6464
Mehrad aminsharei <mehradaminsharei@gmail.com>
2026-01-30 16:09:50 +0330

    added structure comment component

--- src/app/components/Comment/Comments.jsx
+++ src/app/components/Comment/Comments.jsx
@@ -1,7 +1,24 @@
 import styles from "@/app/components/Comment/Comments.module.css";
+import Link from "next/link";
 
 const Comments = () => {
-  return <div className={styles.container}>Comments</div>;
+  const status = "authenticated"; // "unauthenticated"
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Comments</h1>
+      {status === "authenticated" ? (
+        <div className={styles.writeCommentContainer}>
+          <textarea
+            className={styles.textarea}
+            placeholder="Write your comment here..."
+          ></textarea>
+          <button className={styles.sendButton}>Send</button>
+        </div>
+      ) : (
+        <Link href="/login">Login to post a comment</Link>
+      )}
+    </div>
+  );
 };
 
 export default Comments;
